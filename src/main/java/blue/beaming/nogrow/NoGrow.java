5bd8019888ebbfa5cbf3eebeb79501a08847015b
BeamingBlue <beamingblue7@gmail.com>
2022-12-10 23:57:08 +0300

    Update for 1.19.3

--- src/main/java/blue/beaming/nogrow/NoGrow.java
+++ src/main/java/blue/beaming/nogrow/NoGrow.java
@@ -2,30 +2,32 @@ package blue.beaming.nogrow;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.fabricmc.fabric.api.particle.v1.FabricParticleTypes;
+import net.minecraft.block.AbstractBlock.ContextPredicate;
 import net.minecraft.block.DispenserBlock;
 import net.minecraft.block.Material;
-import net.minecraft.block.AbstractBlock.ContextPredicate;
 import net.minecraft.block.dispenser.FallibleItemDispenserBehavior;
-import net.minecraft.block.dispenser.ItemDispenserBehavior;
 import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemGroups;
 import net.minecraft.item.ItemStack;
 import net.minecraft.particle.DefaultParticleType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.math.BlockPointer;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.registry.Registry;
 
 public class NoGrow implements ModInitializer{
     public static final ContextPredicate ALWAYS = (a,b,c) -> true;
     public static final String ID = "nogrow";
-    public static final FermentedBoneMealItem FERMENTED_BONE_MEAL = new FermentedBoneMealItem(new FabricItemSettings().group(ItemGroup.MISC));
+    public static final FermentedBoneMealItem FERMENTED_BONE_MEAL = new FermentedBoneMealItem(new FabricItemSettings());
     public static final DoctoredFarmlandBlock DOCTORED_FARMLAND_BLOCK = new DoctoredFarmlandBlock(FabricBlockSettings.of(Material.SOIL).strength(0.6f).sounds(BlockSoundGroup.GRAVEL).suffocates(ALWAYS).blockVision(ALWAYS));
     public static final DefaultParticleType POISONED_HAPPY_VILLAGER = FabricParticleTypes.simple();
+    public static final BlockItem DOCTORED_FARMLAND_ITEM = new BlockItem(DOCTORED_FARMLAND_BLOCK,new FabricItemSettings());
     @Override
     public void onInitialize(){
         registerItems();
@@ -43,9 +45,11 @@ public class NoGrow implements ModInitializer{
      * Registers block and items.
      */
     private void registerItems(){
-        Registry.register(Registry.ITEM,new Identifier(ID,"fermented_bone_meal"),FERMENTED_BONE_MEAL);
-        Registry.register(Registry.BLOCK,new Identifier(ID, "doctored_farmland"),DOCTORED_FARMLAND_BLOCK);
-        Registry.register(Registry.ITEM,new Identifier(ID, "doctored_farmland"),new BlockItem(DOCTORED_FARMLAND_BLOCK,new FabricItemSettings().group(ItemGroup.DECORATIONS)));
-        Registry.register(Registry.PARTICLE_TYPE, new Identifier(ID, "poisoned_happy_villager"),POISONED_HAPPY_VILLAGER);
+        Registry.register(Registries.ITEM,new Identifier(ID,"fermented_bone_meal"),FERMENTED_BONE_MEAL);
+        Registry.register(Registries.BLOCK,new Identifier(ID, "doctored_farmland"),DOCTORED_FARMLAND_BLOCK);
+        Registry.register(Registries.ITEM,new Identifier(ID,"doctored_farmland"),DOCTORED_FARMLAND_ITEM);
+        Registry.register(Registries.PARTICLE_TYPE, new Identifier(ID, "poisoned_happy_villager"),POISONED_HAPPY_VILLAGER);
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.TOOLS).register(entries -> entries.add(FERMENTED_BONE_MEAL));
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.NATURAL).register(entries -> entries.add(DOCTORED_FARMLAND_ITEM));
     }
 }
--- src/main/java/blue/beaming/nogrow/NoGrowClient.java
+++ src/main/java/blue/beaming/nogrow/NoGrowClient.java
@@ -2,15 +2,11 @@ package blue.beaming.nogrow;
 
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
-import net.fabricmc.fabric.api.event.client.ClientSpriteRegistryCallback;
-import net.minecraft.screen.PlayerScreenHandler;
-import net.minecraft.util.Identifier;
 import net.minecraft.client.particle.SuspendParticle;
 
 public class NoGrowClient implements ClientModInitializer{
     @Override
     public void onInitializeClient(){
-        ClientSpriteRegistryCallback.event(PlayerScreenHandler.BLOCK_ATLAS_TEXTURE).register(((atlasTexture,registry) -> registry.register(new Identifier(NoGrow.ID,"particle/poisoned_happy_villager"))));
         ParticleFactoryRegistry.getInstance().register(NoGrow.POISONED_HAPPY_VILLAGER,SuspendParticle.HappyVillagerFactory::new);
     }
 }
